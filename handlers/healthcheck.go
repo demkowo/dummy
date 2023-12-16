df1e6ccf092ddf1e29b49dbe4f81cf56305c0a9f
demkowo <w.demko@gmail.com>
2023-12-17 00:21:44 +0100

    error descr changes

--- handlers/healthcheck.go
+++ handlers/healthcheck.go
@@ -16,6 +16,7 @@ import (
 var (
 	Healthcheck healthcheckInterface = &healthcheck{}
 	values                           = config.Values.Get()
+	service                          = fmt.Sprintf("service%s", values.PortNumber)
 )
 
 type healthcheckInterface interface {
@@ -30,7 +31,7 @@ type healthcheck struct {
 func (h *healthcheck) Home(w http.ResponseWriter, r *http.Request) {
 	log.Println("--- handler/Home() ---")
 
-	fmt.Fprintf(w, "Service: %s", values.PortNumber)
+	fmt.Fprint(w, "[ok] ", service)
 }
 
 func (h *healthcheck) Host(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +40,7 @@ func (h *healthcheck) Host(w http.ResponseWriter, r *http.Request) {
 	// Extract the port from the URL path
 	pathParts := strings.Split(r.URL.Path, "/")
 	if len(pathParts) > 3 {
-		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		http.Error(w, "[err] Invalid URL", http.StatusBadRequest)
 		return
 	}
 
@@ -48,22 +49,20 @@ func (h *healthcheck) Host(w http.ResponseWriter, r *http.Request) {
 		url := "http://localhost:" + pathParts[2]
 		res, err := http.Get(url)
 		if err != nil {
-			fmt.Fprint(w, "service: ", pathParts[2], ", error: ", err)
+			fmt.Fprint(w, "[err] call service: ", pathParts[2], ", ", err)
 			return
 		}
 		defer res.Body.Close()
 
 		// Check the status code of the response
 		if res.StatusCode != http.StatusOK {
-			fmt.Fprint(w, "Response status:", res.Status)
+			fmt.Fprint(w, "[err] call service: ", pathParts[2], ", ", res.Status)
 			return
 		}
 
-		fmt.Fprint(w, "service: ", pathParts[2], ", status code: ", res.Status)
+		fmt.Fprint(w, "[ok] call service: ", pathParts[2], ", status code: ", res.Status)
 		return
 	}
-
-	fmt.Fprint(w, "ok, ", len(pathParts))
 }
 
 func (h *healthcheck) DB(w http.ResponseWriter, r *http.Request) {
@@ -72,11 +71,11 @@ func (h *healthcheck) DB(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("postgres", values.ConnStr)
 
 	if err != nil {
-		fmt.Fprint(w, "error, can't connect DB")
+		fmt.Fprint(w, "[err] ", service, " can't connect DB")
 		return
 	}
 
 	defer db.Close()
 
-	fmt.Fprint(w, "ok")
+	fmt.Fprint(w, "[ok] ", service, " connected DB")
 }
